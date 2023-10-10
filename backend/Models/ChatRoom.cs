e85ca259112d429f9ac1d56715c3e4e90ca1da22
highsmithcodes <ezrahighsmith@gmail.com>
2023-10-09 21:01:18 -0600

    add CRUD operation

--- backend/Models/ChatRoom.cs
+++
@@ -1,8 +0,0 @@
-public class ChatRoom
-{
-    public int ChatRoomId { get; set; }
-    public string Name { get; set; }
-
-    public ICollection<UserChatRoom> UserChatRooms { get; set; }
-    public ICollection<Message> Messages { get; set; }
-}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,36 +1,11 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
-using System.IO; // Import this for Directory.GetCurrentDirectory()
-using backend.Data;
-using backend.Hubs;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
 
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Configuration for PostgreSQL connection string
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("appsettings.json")
-    .Build();
-
-string connectionString = configuration.GetConnectionString("DefaultConnection"); // "DefaultConnection" should match your connection string key in appsettings.json
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString)); // Use your database context here
-
-// Add SignalR services to the container.
-builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -41,18 +16,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.UseRouting();
-
-app.UseEndpoints(endpoints =>
-{
-    // ... other endpoint mappings ...
-
-    endpoints.MapHub<ChatHub>("/chatHub"); // Map the ChatHub for SignalR
-    endpoints.MapControllers();
-});
+app.MapControllers();
 
 app.Run();
